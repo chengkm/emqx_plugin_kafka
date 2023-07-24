893646b09dc4bbb3bbd26d1703c2be4d2887c029
chengkm <chengzinan1993@gmail.com>
2023-07-24 14:45:48 +0800

    factor: hash改为random

--- src/emqx_plugin_kafka.erl
+++ src/emqx_plugin_kafka.erl
@@ -336,7 +336,7 @@ produce_kafka_payload(Key, Message) ->
   {ok, MessageBody} = emqx_json:safe_encode(Message),
   % ?LOG_INFO("[KAFKA PLUGIN]Message = ~s~n",[MessageBody]),
   Payload = iolist_to_binary(MessageBody),
-  brod:produce_cb(emqx_repost_worker, Topic, hash, Key, Payload, fun(_,_) -> ok end),
+  brod:produce_cb(emqx_repost_worker, Topic, random, Key, Payload, fun(_,_) -> ok end),
   ok.
 
 ntoa({0, 0, 0, 0, 0, 16#ffff, AB, CD}) ->
